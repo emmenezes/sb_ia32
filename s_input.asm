0b76d2897e2c7ce30e3276ef5c2df2646be4b170
Emme Menezes <menezes.ecd@gmail.com>
2022-09-10 15:12:42 -0300

    fix (s_input): melhora espaçamento e corrige ordem na pilha

--- s_input.asm
+++ s_input.asm
@@ -1,39 +1,39 @@
-section .data
-dop2 db 10d
+section     .data
+dop2    dw  10d
 
-section .bss
-dop1 resb 20
+section     .bss
+dop1    resb 20
 
-section .text
+section     .text
 global _start
 _start:
-    push dop1
-    push word 10
-    call s_input
+    push    word    [dop2]
+    push    dop1
+    call    s_input
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, dop1
-    mov edx, 10
-    int 80h
+    mov     eax,    4
+    mov     ebx,    1
+    mov     ecx,    dop1
+    mov     edx,    10
+    int     80h
 
-    mov eax, 1
-    mov ebx, 0
-    int 80h
+    mov     eax,    1
+    mov     ebx,    0
+    int     80h
 
 s_input:
-    push ebp
-    mov ebp, esp
-    push eax
-    sub edx, edx
+    push    ebp
+    mov     ebp,    esp
+    push    eax
+    sub     edx,    edx
     
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, [ebp+10]
-    mov dx, [ebp+8]
+    mov     eax,    3
+    mov     ebx,    0
+    mov     ecx,    [ebp+8]
+    mov     dx,     [ebp+12]
 k:
-    int 80h
+    int     80h
 
-    pop eax
-    pop ebp
-    ret 10
+    pop     eax
+    pop     ebp
+    ret     10
